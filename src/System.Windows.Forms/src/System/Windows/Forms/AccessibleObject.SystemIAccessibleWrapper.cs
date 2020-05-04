dab29ead771d1a18abbb0323666f782e6a681109
Mikhail Lipin <v-milipi@microsoft.com>
2020-05-04 13:49:42 +0300

    Fixing code-style issues.

--- src/System.Windows.Forms/src/System/Windows/Forms/AccessibleObject.SystemIAccessibleWrapper.cs
+++ src/System.Windows.Forms/src/System/Windows/Forms/AccessibleObject.SystemIAccessibleWrapper.cs
@@ -26,14 +26,10 @@ namespace System.Windows.Forms
             private IAccessible _systemIAccessible;
 
             public SystemIAccessibleWrapper(IAccessible systemIAccessible)
-            {
-                _systemIAccessible = systemIAccessible;
-            }
+                => _systemIAccessible = systemIAccessible;
 
             public void accSelect(int flagsSelect, object varChild)
-            {
-                Execute(() => _systemIAccessible.accSelect(flagsSelect, varChild));
-            }
+                => Execute(() => _systemIAccessible.accSelect(flagsSelect, varChild));
 
             public void accLocation(out int pxLeft, out int pyTop, out int pcxWidth, out int pcyHeight, object varChild)
             {
@@ -60,118 +56,96 @@ namespace System.Windows.Forms
             }
 
             public object accNavigate(int navDir, object varStart)
-            {
-                return GetValue(() => _systemIAccessible.accNavigate(navDir, varStart));
-            }
+                => GetValue(() => _systemIAccessible.accNavigate(navDir, varStart));
 
             public object accHitTest(int xLeft, int yTop)
-            {
-                return GetValue(() => _systemIAccessible.accHitTest(xLeft, yTop));
-            }
+                => GetValue(() => _systemIAccessible.accHitTest(xLeft, yTop));
 
             public void accDoDefaultAction(object varChild)
-            {
-                Execute(() => _systemIAccessible.accDoDefaultAction(varChild));
-            }
+                => Execute(() => _systemIAccessible.accDoDefaultAction(varChild));
 
-            public object accParent => GetValue(() => _systemIAccessible.accParent);
+            public object accParent
+                => GetValue(() => _systemIAccessible.accParent);
 
-            public int accChildCount => GetStructValue(() => _systemIAccessible.accChildCount);
+            public int accChildCount
+                => GetValue(() => _systemIAccessible.accChildCount);
 
-            public object get_accChild(object childID) =>
-                GetValue(() => _systemIAccessible.get_accChild(childID));
+            public object get_accChild(object childID)
+                => GetValue(() => _systemIAccessible.get_accChild(childID));
 
             public string get_accName(object childID)
-            {
-                return GetValue(() => _systemIAccessible.get_accName(childID));
-            }
+                => GetValue(() => _systemIAccessible.get_accName(childID));
 
             public void set_accName(object childID, string newName)
-            {
-                Execute(() => _systemIAccessible.set_accName(childID, newName));
-            }
+                => Execute(() => _systemIAccessible.set_accName(childID, newName));
 
             public string accName
             {
                 get => GetValue(() => _systemIAccessible.accName);
-                set => Execute(() =>
-                {
-                    _systemIAccessible.accName = value;
-                });
+                set => Execute(() => _systemIAccessible.accName = value);
             }
 
             public string accValue
             {
                 get => GetValue(() => _systemIAccessible.accValue);
-                set => Execute(() =>
-                {
-                    _systemIAccessible.accValue = value;
-                });
+                set => Execute(() => _systemIAccessible.accValue = value);
             }
 
             public string get_accValue(object childID)
-            {
-                return GetValue(() => _systemIAccessible.get_accValue(childID));
-            }
+                => GetValue(() => _systemIAccessible.get_accValue(childID));
 
             public void set_accValue(object childID, string newValue)
-            {
-                Execute(() => _systemIAccessible.set_accValue(childID, newValue));
-            }
+                => Execute(() => _systemIAccessible.set_accValue(childID, newValue));
 
-            public string accDescription => GetValue(() => _systemIAccessible.accDescription);
+            public string accDescription
+                => GetValue(() => _systemIAccessible.accDescription);
 
             public string get_accDescription(object childID)
-            {
-                return GetValue(() => _systemIAccessible.get_accDescription(childID));
-            }
+                => GetValue(() => _systemIAccessible.get_accDescription(childID));
 
-            public object accRole => GetValue(() => _systemIAccessible.accRole);
+            public object accRole
+                => GetValue(() => _systemIAccessible.accRole);
 
             public object get_accRole(object childID)
-            {
-                return GetValue(() => _systemIAccessible.get_accRole(childID));
-            }
+                => GetValue(() => _systemIAccessible.get_accRole(childID));
 
-            public object accState => GetValue(() => _systemIAccessible.accState);
+            public object accState
+                => GetValue(() => _systemIAccessible.accState);
 
             public object get_accState(object childID)
-            {
-                return GetValue(() => _systemIAccessible.get_accState(childID));
-            }
+                => GetValue(() => _systemIAccessible.get_accState(childID));
 
-            public string accHelp => GetValue(() => _systemIAccessible.accHelp);
+            public string accHelp
+                => GetValue(() => _systemIAccessible.accHelp);
 
             public string get_accHelp(object childID)
-            {
-                return GetValue(() => _systemIAccessible.get_accHelp(childID));
-            }
+                => GetValue(() => _systemIAccessible.get_accHelp(childID));
 
             public int get_accHelpTopic(out string pszHelpFile, object childID)
             {
                 string helpFile = null;
-                int result = GetStructValue(() => _systemIAccessible.get_accHelpTopic(out helpFile, childID), -1);
+                int result = GetValue(() => _systemIAccessible.get_accHelpTopic(out helpFile, childID), -1);
                 pszHelpFile = helpFile;
                 return result;
             }
 
-            public string accKeyboardShortcut => GetValue(() => _systemIAccessible.accKeyboardShortcut);
+            public string accKeyboardShortcut
+                => GetValue(() => _systemIAccessible.accKeyboardShortcut);
 
             public string get_accKeyboardShortcut(object childID)
-            {
-                return GetValue(() => _systemIAccessible.get_accKeyboardShortcut(childID));
-            }
+                => GetValue(() => _systemIAccessible.get_accKeyboardShortcut(childID));
 
-            public object accFocus => GetValue(() => _systemIAccessible.accFocus);
+            public object accFocus
+                => GetValue(() => _systemIAccessible.accFocus);
 
-            public object accSelection => GetValue(() => _systemIAccessible.accSelection);
+            public object accSelection
+                => GetValue(() => _systemIAccessible.accSelection);
 
-            public string accDefaultAction => GetValue(() => _systemIAccessible.accDefaultAction);
+            public string accDefaultAction
+                => GetValue(() => _systemIAccessible.accDefaultAction);
 
             public string get_accDefaultAction(object childID)
-            {
-                return GetValue(() => _systemIAccessible.get_accDefaultAction(childID));
-            }
+                => GetValue(() => _systemIAccessible.get_accDefaultAction(childID));
 
             private TReturn GetValue<TReturn>(Func<TReturn> getFunction) where TReturn : class
             {
@@ -197,7 +171,7 @@ namespace System.Windows.Forms
                 return null;
             }
 
-            private TReturn GetStructValue<TReturn>(
+            private TReturn GetValue<TReturn>(
                 Func<TReturn> func,
                 TReturn defaultReturnValue = default(TReturn)) where TReturn : struct
             {
