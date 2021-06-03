dccc3deee58bd25b9f5abbb5c01c36520f45e47d
Igor Velikorossov <igor.velikorossov@microsoft.com>
2021-06-03 14:33:26 +1000

    Add source generator

---
+++ src/System.Windows.Forms.Analyzers.CSharp/src/System/Windows/Forms/Generators/AnalyzerConfigOptionsProviderExtensions.cs
@@ -0,0 +1,25 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.Diagnostics;
+
+namespace System.Windows.Forms.Analyzers.Generators
+{
+    internal static class AnalyzerConfigOptionsProviderExtensions
+    {
+        /// <summary>
+        /// Attempts to read a value for the requested MSBuild property.
+        /// </summary>
+        /// <param name="analyzerConfigOptions">The global optins.</param>
+        /// <param name="name">The name of the property to read the value for.</param>
+        /// <param name="defaultValue">The value to return, if property is absent.</param>
+        /// <returns></returns>
+        public static string GetMSBuildProperty(this AnalyzerConfigOptionsProvider analyzerConfigOptions, string name, string defaultValue = "")
+        {
+            analyzerConfigOptions.GlobalOptions.TryGetValue($"build_property.{name}", out var value);
+            return value ?? defaultValue;
+        }
+    }
+}
---
+++ src/System.Windows.Forms.Analyzers.CSharp/src/System/Windows/Forms/Generators/ApplicationConfigurationGenerator.cs
@@ -0,0 +1,63 @@
+﻿// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+// See the LICENSE file in the project root for more information.
+
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+
+namespace System.Windows.Forms.Analyzers.Generators
+{
+    [Generator]
+    internal class ApplicationConfigurationGenerator : ISourceGenerator
+    {
+        public void Execute(GeneratorExecutionContext context)
+        {
+            if (context.SyntaxReceiver is not ApplicationConfigurationSyntaxReceiver syntaxReceiver)
+            {
+                throw new InvalidOperationException("We were given the wrong syntax receiver.");
+            }
+
+            if (syntaxReceiver.Nodes.Count == 0)
+            {
+                return;
+            }
+
+            if (context.Compilation.Options.OutputKind != OutputKind.WindowsApplication)
+            {
+                context.ReportDiagnostic(Diagnostic.Create(DiagnosticDescriptors.s_errorUnsupportedProjectType, Location.None));
+                return;
+            }
+
+            ApplicationConfig? projectConfig = ProjectFileReader.ReadApplicationConfig(context);
+            if (projectConfig is null)
+            {
+                return;
+            }
+
+            string? code = ApplicationConfigurationInitializeBuilder.GenerateInitialize(projectNamespace: GetUserProjectNamespace(syntaxReceiver.Nodes[0]), projectConfig);
+            if (code is not null)
+            {
+                context.AddSource("ApplicationConfiguration.g.cs", code);
+            }
+        }
+
+        private string GetUserProjectNamespace(SyntaxNode node)
+        {
+            string ns = "SourceGenerated";
+
+            // TODO: what namespace do top-level programs have?
+            if (node.Ancestors().FirstOrDefault(a => a is NamespaceDeclarationSyntax) is NamespaceDeclarationSyntax namespaceSyntax)
+            {
+                ns = namespaceSyntax.Name.ToString();
+            }
+
+            return ns;
+        }
+
+        public void Initialize(GeneratorInitializationContext context)
+        {
+            context.RegisterForSyntaxNotifications(() => new ApplicationConfigurationSyntaxReceiver());
+        }
+    }
+}
